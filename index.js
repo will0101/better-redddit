f4eb1f5d672c0bc19a0c685631d0d4d9d4c7235a
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-11-05 22:48:58 -0500

    added getURL function

--- index.js
+++ index.js
@@ -85,3 +85,37 @@ exports.search = (query,cb) => {
 		}
 	})
 }
+
+exports.getURL = (url, param, cb) => {
+	if (!url) {
+		var err = {
+			"err": {
+				"message": "Query parameter required.",
+				"code": "noParam"
+			}
+		}
+		return cb(err, null);
+	}
+	if (!cb) {
+		var cb = param;
+		var param = "";
+	}
+	var opt = { headers: {
+		"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+		"Accept-Encoding":"gzip, deflate, br",
+		"Connection":"keep-alive",
+		"DNT":"1",
+		"Host":"www.reddit.com",
+		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+	}}
+	needle.get("https://reddit.com" + url + ".json" + param,opt,async function(err,resp,body) {
+		if (err) {
+			var body = null;
+			cb(err,body);
+		} else {
+			var err = null;
+			var res = body.data.children;
+			cb(err,res)
+		}
+	})
+}
